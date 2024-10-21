ef4f656c8b6264377b573dd83038baf8b087cb23
efrontinan <elenauxui@gmail.com>
2024-10-21 14:53:41 +0200

    Day 1 - Solved

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,71 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
 
     // 2. getQuestion()
     
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+    }
+
     // 3. moveToNextQuestion()
+
+    moveToNextQuestion(){
+        this.currentQuestionIndex ++
+        return this.questions[this.currentQuestionIndex]
+
+    }
 
     // 4. shuffleQuestions()
 
+    shuffleQuestions(){
+        let currentIndex = this.questions.length;
+      
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element...
+          let randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [this.questions[currentIndex], this.questions[randomIndex]] = [
+            this.questions[randomIndex], this.questions[currentIndex]];
+        }
+    }
+
     // 5. checkAnswer(answer)
 
+    checkAnswer(answer){
+        this.questions.forEach((eachQuestion) => {
+            console.log (eachQuestion)
+            if(answer === eachQuestion.answer){
+                this.correctAnswers ++
+        }
+        })
+    }
+
     // 6. hasEnded()
+
+    hasEnded(){
+        if(this.currentQuestionIndex === this.questions.length){
+            return true
+        } else if ( this.currentQuestionIndex < this.questions.length){
+            return false
+        }
+        // this.questions.forEach((eachQuestion) => {
+        //     if (this.currentQuestionIndex === eachQuestion.length){
+        //         return true
+        //     } else if (this.currentQuestionIndex < eachQuestion.length) {
+        //         return false
+        //     }
+        // })
+    }
 }
