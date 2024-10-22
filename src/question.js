a8cf4ee5cd69f1609a561e3040e4a0513a10f777
DieSILV <59703636+DieSILV@users.noreply.github.com>
2024-10-22 12:28:35 +0200

    Corrections day 1

--- src/question.js
+++ src/question.js
@@ -1,28 +1,14 @@
 class Question {
-    // YOUR CODE HERE:
-    constructor(text, choices, answer, difficulty){
-        this.text = text
-        this.choices = choices
-        this.answer = answer
-        this.difficulty = difficulty
-    }
 
-    // 2. shuffleChoices()
+  constructor(text, choices, answer, difficulty) {
+    this.text = text
+    this.choices = choices
+    this.answer = answer
+    this.difficulty = difficulty
+  }
 
-    shuffleChoices() {
-        let currentIndex = this.choices.length;
-      
-        // While there remain elements to shuffle...
-        while (currentIndex != 0) {
-      
-          // Pick a remaining element...
-          let randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-      
-          // And swap it with the current element.
-          [this.choices[currentIndex], this.choices[randomIndex]] = [
-            this.choices[randomIndex], this.choices[currentIndex]];
-        }
-      }
+  shuffleChoices() {
+    this.choices.sort(() => .5 - Math.random());
+  }
 
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,7 +1,5 @@
 class Quiz {
-    // YOUR CODE HERE:
-
-    constructor(questions, timeLimit, timeRemaining){
+    constructor(questions, timeLimit, timeRemaining) {
         this.questions = questions
         this.timeLimit = timeLimit
         this.timeRemaining = timeRemaining
@@ -9,75 +7,26 @@ class Quiz {
         this.currentQuestionIndex = 0
     }
 
-    // 2. getQuestion()
-    
-    getQuestion(){
+    getQuestion() {
         return this.questions[this.currentQuestionIndex]
     }
 
-    // 3. moveToNextQuestion()
-
-    moveToNextQuestion(){
-        this.currentQuestionIndex ++
-
+    moveToNextQuestion() {
+        this.currentQuestionIndex++
     }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        this.questions.sort(() => .5 - Math.random());
+    }
 
-    shuffleQuestions(){
-        let currentIndex = this.questions.length;
-      
-        // While there remain elements to shuffle...
-        while (currentIndex != 0) {
-      
-          // Pick a remaining element...
-          let randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-      
-          // And swap it with the current element.
-          [this.questions[currentIndex], this.questions[randomIndex]] = [
-            this.questions[randomIndex], this.questions[currentIndex]];
+    checkAnswer(answerToCheck) {
+        const correctAnswer = this.getQuestion().answer
+        if (answerToCheck === correctAnswer) {
+            this.correctAnswers++
         }
     }
 
-    // 5. checkAnswer(answer)
-
-
-
-    checkAnswer(answer){
-
-        
-      if (answer ===  this.getQuestion().answer) {
-        this.correctAnswers ++
-      }
-
-
-        // console.log(this.questions)
-        
-        // this.questions.forEach((eachQuestion) => {
-        //     console.log (eachQuestion)
-        //     console.log(answer)
-        //     if(answer === eachQuestion.answer){
-        //         this.correctAnswers ++
-        //         console.log (this.correctAnswers)
-        // }
-        // })
-    }
-
-    // 6. hasEnded()
-
-    hasEnded(){
-        if(this.currentQuestionIndex === this.questions.length){
-            return true
-        } else if ( this.currentQuestionIndex < this.questions.length){
-            return false
-        }
-        // this.questions.forEach((eachQuestion) => {
-        //     if (this.currentQuestionIndex === eachQuestion.length){
-        //         return true
-        //     } else if (this.currentQuestionIndex < eachQuestion.length) {
-        //         return false
-        //     }
-        // })
+    hasEnded() {
+        return this.currentQuestionIndex >= this.questions.length
     }
 }
