3f4195f7c29f6ec532ab5c4636a53b5a4de9c326
DieSILV <59703636+DieSILV@users.noreply.github.com>
2024-10-22 13:09:52 +0200

    Day 2 - Solved

--- src/quiz.js
+++ src/quiz.js
@@ -29,4 +29,38 @@ class Quiz {
     hasEnded() {
         return this.currentQuestionIndex >= this.questions.length
     }
+
+    filterQuestionsByDifficulty(difficulty) {
+        // console.log('Antes', this.questions)
+        // console.log(difficulty)
+        // const filteredQuestions = this.questions.filter(eachQuestion => {
+        //     return eachQuestion.difficulty === difficulty
+        // })
+
+        // if (difficulty >= 1 && difficulty <= 3) {
+        //     this.questions = filteredQuestions
+        // }
+
+        // console.log('Despues', this.questions)
+
+        //console.log('Antes', this.questions)
+
+        if (difficulty >= 1 && difficulty <= 3) {
+            this.questions = this.questions.filter(eachQuestion => {
+                return eachQuestion.difficulty === difficulty
+            })
+        }
+
+        //console.log('Despues', this.questions)
+
+    }
+
+    averageDifficulty() {
+
+        const sumDifficulty = this.questions.reduce((acc, eachQuestions) => {
+            return acc + eachQuestions.difficulty
+        }, 0)
+
+        return sumDifficulty / this.questions.length
+    }
 }
